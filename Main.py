d99a67fd9a9b5eab29ad1a2d11b037dacbba2f37
niewiel <tniewiel@gmail.com>
2017-05-22 19:05:09 +0200

    final

--- Main.py
+++ Main.py
@@ -1,43 +1,31 @@
 import pygame, time
 
-# zmienne globalne
-okno_otwarte = True
-plik = 'statek.png'
-plik_obcy = 'obcy.png'
-życie = 3
-poziom = 1
-gra_aktywna = False
-
-# stałe
-ROZMIAR = SZEROKOŚĆ, WYSOKOŚĆ = (800, 600)
-BIAŁY = pygame.color.THECOLORS['white']
-CZARNY = pygame.color.THECOLORS['black']
-CIEMNOCZERWONY = pygame.color.THECOLORS['darkred']
-JASNONIEBIESKI = pygame.color.THECOLORS['lightblue']
-obraz = pygame.image.load(plik)
-mini_obraz = pygame.transform.scale(obraz, [obraz.get_rect().width // 3,
-                                            obraz.get_rect().height // 3])
-
-# ustawienia okna i gry
 pygame.init()
 
+# STAŁE
+ROZMIAR = SZEROKOŚĆ, WYSOKOŚĆ = 800, 600
+BIAŁY = pygame.color.THECOLORS['white']
+CIEMNOCZERWONY = pygame.color.THECOLORS['darkred']
+CIEMNOZIELONY = pygame.color.THECOLORS['darkgreen']
+CZARNY = pygame.color.THECOLORS['black']
+SZARY = pygame.color.THECOLORS['gray']
+plik = 'statek.png'
+plik_obcy = 'obcy.png'
+obraz = pygame.image.load(plik)
+mini_obraz = pygame.transform.scale(obraz, (obraz.get_rect().width // 3,
+                                            obraz.get_rect().height // 3))
+
+# zmienne globalne
+okno_otwarte = True
+życia = 3
+poziom = 1
+prędkość = 20
+start = True
+gra_aktywna = False
+
 ekran = pygame.display.set_mode(ROZMIAR)
-pygame.display.set_caption('Prosta gra.')
+pygame.display.set_caption('Moja gra...')
 zegar = pygame.time.Clock()
-
-
-class Obcy(pygame.sprite.Sprite):
-    def __init__(self, plik_obrazu):
-        super().__init__()
-        self.image = pygame.image.load(plik_obrazu)
-        self.rect = self.image.get_rect()
-        self.ruch_x = 1
-
-    def update(self):
-        self.rect.x += self.ruch_x
-
-    def draw(self, surface):
-        surface.blit(self.image, self.rect)
 
 
 class Statek(pygame.sprite.Sprite):
@@ -47,24 +35,14 @@ class Statek(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.ruch_x = 0
 
-    def skręt_w_lewo(self):
+    def skreć_w_lewo(self):
         self.ruch_x = -6
 
-    def skręt_w_prawo(self):
+    def skreć_w_prawo(self):
         self.ruch_x = 6
 
     def stop(self):
         self.ruch_x = 0
-
-    def strzał(self, pociski):
-        if len(pociski) < 4:
-            pocisk = Pocisk()
-            pocisk.rect.centerx = self.rect.centerx
-            pocisk.rect.bottom = self.rect.top
-            pociski.add(pocisk)
-
-    def draw(self, surface):
-        surface.blit(self.image, self.rect)
 
     def update(self):
         self.rect.x += self.ruch_x
@@ -73,14 +51,24 @@ class Statek(pygame.sprite.Sprite):
         if self.rect.left < 0:
             self.rect.left = 0
 
-    def obsługa_zdarzeń(self, event, pociski):
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+    def strzał(self, grupa_pocisków):
+        if len(grupa_pocisków) < 4:
+            pocisk = Pocisk()
+            pocisk.rect.centerx = self.rect.centerx
+            pocisk.rect.bottom = self.rect.top
+            grupa_pocisków.add(pocisk)
+
+    def reakcja_na_zdarzenia(self, event, grupa_pocisków):
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_a:
-                self.skręt_w_lewo()
+                self.skreć_w_lewo()
             if event.key == pygame.K_d:
-                self.skręt_w_prawo()
+                self.skreć_w_prawo()
             if event.key == pygame.K_SPACE:
-                self.strzał(pociski)
+                self.strzał(grupa_pocisków)
 
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_a and self.ruch_x < 0:
@@ -89,21 +77,43 @@ class Statek(pygame.sprite.Sprite):
                 self.stop()
 
 
+class Obcy(pygame.sprite.Sprite):
+    def __init__(self, plik_obrazu, ruch_x=1):
+        super().__init__()
+        self.image = pygame.image.load(plik_obrazu)
+        self.rect = self.image.get_rect()
+        self.ruch_x = ruch_x
+
+    def sprawdź_krawędzie_boczne(self):
+        if self.rect.left < 0 or self.rect.right > SZEROKOŚĆ:
+            return True
+        else:
+            return False
+
+    def sprawdź_krawędź_dolną(self, statek):
+        if self.rect.bottom > WYSOKOŚĆ - statek.rect.height:
+            return True
+        else:
+            return False
+
+    def update(self):
+        self.rect.x += self.ruch_x
+
+
 class Pocisk(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = pygame.Surface([4, 10])
-        self.image.fill(CIEMNOCZERWONY)
         self.rect = self.image.get_rect()
         self.ruch_y = 7
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
 
     def update(self):
         self.rect.y -= self.ruch_y
         if self.rect.bottom < 0:
             self.kill()
-
-    def draw(self, surface):
-        surface.blit(self.image, self.rect)
 
 
 class Tekst:
@@ -124,19 +134,20 @@ class Tekst:
 
 
 class Przycisk:
-    def __init__(self, tekst, szerokość, wysokość, kolor_tekstu, kolor_tła):
+    def __init__(self, tekst, szerokość, wysokość, kolor_tła, kolor_tekstu):
         self.tekst = tekst
         self.szerokość = szerokość
         self.wysokość = wysokość
-        self.kolor_tekstu = kolor_tekstu
         self.kolor_tła = kolor_tła
+        self.kolor_tekstu = kolor_tekstu
         self.font = pygame.font.SysFont(None, 60)
         self.rect = pygame.Rect(0, 0, self.szerokość, self.wysokość)
-        self.rect.center = [SZEROKOŚĆ / 2, WYSOKOŚĆ / 2]
+        self.rect.center = [SZEROKOŚĆ // 2, WYSOKOŚĆ // 2]
         self.ustaw()
 
     def ustaw(self):
-        self.image = self.font.render(self.tekst, 1, self.kolor_tekstu, self.kolor_tła)
+        self.image = self.font.render(self.tekst,
+                                      1, self.kolor_tekstu, self.kolor_tła)
         self.rect_image = self.image.get_rect()
         self.rect_image.center = self.rect.center
 
@@ -145,37 +156,60 @@ class Przycisk:
         surface.blit(self.image, self.rect_image)
 
 
-def stwórz_obcych(statek):
+def stwórz_obcych(prędkość=1):
     grupa_obcych = pygame.sprite.Group()
     obcy = Obcy(plik_obcy)
     szerokość = SZEROKOŚĆ - 2 * obcy.rect.width
     liczba_obcych_w_rzędzie = int(szerokość / (2 * obcy.rect.width))
     wysokość = WYSOKOŚĆ - 3 * obcy.rect.height - statek.rect.height
-    liczba_obcych_w_kolumnie = int(wysokość / (2 * obcy.rect.height))
-    for r in range(liczba_obcych_w_kolumnie):
-        for p in range(liczba_obcych_w_rzędzie):
-            obcy = Obcy(plik_obcy)
-            obcy.rect.x = obcy.rect.width + 2 * obcy.rect.width * p
-            obcy.rect.y = obcy.rect.height + 2 * obcy.rect.height * r
+    liczba_rzędów = int(wysokość / (2 * obcy.rect.height))
+    for i in range(liczba_rzędów):
+        for j in range(liczba_obcych_w_rzędzie):
+            obcy = Obcy(plik_obcy, prędkość)
+            obcy.rect.x = obcy.rect.width + 2 * obcy.rect.width * j
+            obcy.rect.y = obcy.rect.height + 2 * obcy.rect.height * i
+
             grupa_obcych.add(obcy)
 
     return grupa_obcych
 
 
-def restart_gry(ekran, statek):
+def reset_planszy(statek, ekran):
     global grupa_obcych
     time.sleep(0.5)
     ekran.fill(BIAŁY)
-    poziom_tekst = 'POZIOM {} '.format(poziom)
+    obiekt_poziom.tekst = "POZIOM {}".format(poziom)
+    obiekt_poziom.ustaw([SZEROKOŚĆ // 2, WYSOKOŚĆ // 2])
+    obiekt_poziom.draw(ekran)
+    pygame.display.flip()
+    time.sleep(0.5)
+    grupa_obcych.empty()
+    grupa_pocisków.empty()
+    grupa_obcych = stwórz_obcych(prędkość)
+    statek.rect.centerx = SZEROKOŚĆ // 2
+    statek.rect.bottom = WYSOKOŚĆ
+    ekran.fill(BIAŁY)
+    statek.draw(ekran)
+    grupa_obcych.draw(ekran)
+    obiekt_punkty.draw(ekran)
+    # rysowanie żyć
+    if życia:
+        for i in range(życia):
+            ekran.blit(mini_obraz, [40 * i - 40, 15])
+
+    pygame.display.flip()
+    time.sleep(0.5)
 
 
-# konkretyzacja obiektów
+# konkretyzacja obiektu
 statek = Statek(plik)
 statek.rect.centerx = SZEROKOŚĆ // 2
 statek.rect.bottom = WYSOKOŚĆ
 grupa_pocisków = pygame.sprite.Group()
-grupa_obcych = stwórz_obcych(statek)
-obiekt_punkty = Tekst(0, CIEMNOCZERWONY, [SZEROKOŚĆ // 2, 15])
+grupa_obcych = stwórz_obcych(10)
+obiekt_punkty = Tekst(0, CIEMNOZIELONY, [SZEROKOŚĆ // 2, 20])
+obiekt_poziom = Tekst(0, CZARNY, [SZEROKOŚĆ // 2, WYSOKOŚĆ // 2], 56)
+przycisk = Przycisk("START", 300, 100, SZARY, CZARNY)
 
 # pętla gry
 while okno_otwarte:
@@ -184,54 +218,75 @@ while okno_otwarte:
         if event.type == pygame.QUIT:
             okno_otwarte = False
         else:
-            statek.obsługa_zdarzeń(event, grupa_pocisków)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if przycisk.rect.collidepoint(pygame.mouse.get_pos()):
+                    gra_aktywna = True
+                    pygame.mouse.set_visible(False)
+            else:
+                statek.reakcja_na_zdarzenia(event, grupa_pocisków)
 
-    ekran.fill(BIAŁY)
-    statek.update()
-    statek.draw(ekran)
-    grupa_pocisków.update()
-    for p in grupa_pocisków:
-        trafione = pygame.sprite.spritecollide(p, grupa_obcych, True)
-        if trafione:
-            p.kill()
+    if gra_aktywna:
+        if start:
+            reset_planszy(statek, ekran)
+            start = False
+        ekran.fill(BIAŁY)
+        statek.update()
+        statek.draw(ekran)
+        grupa_pocisków.update()
+        grupa_pocisków.draw(ekran)
+        grupa_obcych.update()
+        for ob in grupa_obcych:
+            if ob.sprawdź_krawędzie_boczne():
+                for o in grupa_obcych:
+                    o.image = pygame.transform.flip(o.image, True, False)
+                    o.ruch_x *= -1
+                    o.rect.y += 10
+                break
 
-    grupa_pocisków.draw(ekran)
+        grupa_obcych.draw(ekran)
+        # rysowanie i aktualizacja punktów
+        for p in grupa_pocisków:
+            grupa_trafionych = pygame.sprite.spritecollide(p, grupa_obcych, True)
+            for t in grupa_trafionych:
+                obiekt_punkty.tekst += 50
+                obiekt_punkty.ustaw([SZEROKOŚĆ // 2, 20])
 
-    # rysowanie i aktualizacja punktów
-    for p in grupa_pocisków:
-        grupa_trafionych = pygame.sprite.spritecollide(p, grupa_obcych, True)
-        for t in grupa_trafionych:
-            obiekt_punkty.tekst += 50
-            obiekt_punkty.ustaw([SZEROKOŚĆ // 2, 20])
+                p.kill()
 
-            p.kill()
+        obiekt_punkty.draw(ekran)
 
-    obiekt_punkty.draw(ekran)
+        # rysowanie żyć
+        if życia:
+            for i in range(życia):
+                ekran.blit(mini_obraz, [40 * i - 40, 15])
 
-    # rysowanie żyć
-    if życie:
-        for i in range(życie):
-            ekran.blit(mini_obraz, [40 * i - 40, 15])
+        for ob in grupa_obcych:
+            if ob.sprawdź_krawędź_dolną(statek):
+                życia -= 1
+                if życia:
+                    reset_planszy(statek, ekran)
+                break
 
-    for ob in grupa_obcych:
-        if ob.rect.bottom > WYSOKOŚĆ - statek.rect.height:
-            życie -= 1
-            break
+        if not życia:
+            okno_otwarte = False
 
-    obiekt_punkty.draw(ekran)
+        if len(grupa_obcych) == 0:
+            poziom += 1
+            prędkość += 1
+            reset_planszy(statek, ekran)
 
-    grupa_obcych.update()
-    for ob in grupa_obcych:
-        if ob.rect.right > SZEROKOŚĆ or ob.rect.left < 0:
-            for ob in grupa_obcych:
-                ob.ruch_x *= -1
-                ob.image = pygame.transform.flip(ob.image, True, False)
-                ob.rect.y += 5
-            break
+        pygame.display.flip()
+        zegar.tick(30)
+    else:
+        przycisk.draw(ekran)
+        pygame.display.flip()
 
-    grupa_obcych.draw(ekran)
-
-    pygame.display.flip()
-    zegar.tick(30)
+time.sleep(0.5)
+ekran.fill(BIAŁY)
+obiekt_poziom.tekst = "KONIEC GRY"
+obiekt_poziom.ustaw([SZEROKOŚĆ // 2, WYSOKOŚĆ // 2])
+obiekt_poziom.draw(ekran)
+pygame.display.flip()
+time.sleep(0.5)
 
 pygame.quit()
